77d2ed3213fb16a51cf8a8fb76e3ee85a006bddc
Al <klimoff.81@mail.ru>
2025-01-27 22:47:22 +0300

    Решение третьей задачи по переменным

--- app.js
+++ app.js
@@ -12,3 +12,9 @@ alert(admin);
 
 let ourPlanetName = "Земля"
 let currentUserName = "Герасим"
+
+// Задача 3 по переменным 
+
+const BIRTHDAY = '18.04.1982'; // здесь допустимо, тк известно заранее
+
+const age = someCode(birthday); // здесь вычисляемое в ходе скрипта, заглавные не допускаются
