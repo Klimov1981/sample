6309d02914bb0794333a47effdfc8de82630b7dc
Al <klimoff.81@mail.ru>
2025-02-01 23:10:06 +0300

    простая задача

--- src/obj.js
+++ src/obj.js
@@ -1,19 +1,44 @@
-// при условии, что ззранее известно - только числовые свойства 
-const myObj = {
-  num: 3,
-  copies: 8,
-  price: 38,
-}
-
 function myObjSum(obj){
   let sum = 0
-  for(key in obj){
-    sum = sum + obj[key]
+
+  // объект ли obj?
+
+  if(typeof obj === 'object' && typeof obj !== null) {
+    // проходим циклом по свойствам
+    for(key in obj){
+      //есть ли вложенные свойства
+      if( obj.hasOwnProperty(key)){
+        // сознаняем своство в value
+        const value = obj[key]
+        // дальше проверяем число в этом свойстве
+
+        if(typeof value === 'number'){
+          //добавляем к сумме
+          sum += value
+          // если это объект вызываем рекурсивно этот метод для этого объекта из value и добавляем результат к сумме
+        } else if (typeof value === 'object') {
+          sum += myObjSum(value)
+        }
+      }
+    }
   }
   return sum
 }
 
-console.log(myObjSum(myObj))
+// тестовый объект 
 
+const testObj = {
+  a: 10,
+  b: "hello",
+  c: {
+      d: 5,
+      e: "world",
+      f: {
+          g: 7,
+          h: [3, 4, "test", { i: 2 }]
+      }
+  },
+  j: [1, 2, "string", null, { k: 8 }]
+};
 
-
+console.log(myObjSum(testObj)); // 40
